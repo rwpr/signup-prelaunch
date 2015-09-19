42df419186038b316d1c649feb7bcff2f7ebe952
rwpr <ruuiwong@gmail.com>
2015-09-19 18:57:16 +0800

    changed yml file

--- app/models/visitor.rb
+++ app/models/visitor.rb
@@ -1,6 +1,7 @@
 class Visitor < ActiveRecord::Base
   validates_presence_of :email
   validates_format_of :email, :with => /\A[-a-z0-9_+\.]+\@([-a-z0-9]+\.)+[a-z0-9]{2,4}\z/i
+  validates :email, uniqueness: true
   after_create :subscribe
 
   def subscribe
